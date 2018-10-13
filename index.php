0daf21e566b0345809dcd8565822bc9d3638a935
Marcelo Souza Vasconcelos <marcelosouza_chan@yahoo.com.br>
2018-10-13 19:55:36 -0300

    criando o page admin e o tpl admin

--- index.php
+++ index.php
@@ -5,15 +5,25 @@ require_once("vendor/autoload.php");
 //nome das classes que vou usar do verndor
 use \Slim\Slim;
 use \Sowlife\Page;
+use \Sowlife\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-//Rotas para serem usadas
+//Rotas para serem usadas no site
 $app->get('/', function() {
     
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+//Rotas para serem usadas
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
