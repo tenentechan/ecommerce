42e7317010df6a7252f16c5a9c657dfeee33b259
Marcelo Souza Vasconcelos <marcelosouza_chan@yahoo.com.br>
2018-10-13 21:51:54 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 //nome das classes que vou usar do verndor
 use \Slim\Slim;
 use \Sowlife\Page;
 use \Sowlife\PageAdmin;
+use \Sowlife\Model\User;
 
 $app = new Slim();
 
@@ -20,14 +21,49 @@ $app->get('/', function() {
 
 });
 
-//Rotas para serem usadas
+//Rotas para serem usadas no admin
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
 });
+
+//Rotas para serem usadas no login
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+
+	]);
+
+	$page->setTpl("login");
+
+});
+
+//Rotas para acessar o login enviar formulario
+$app->post('/admin/login', function() {
+    
+    User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+	});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
+
+	
 
 $app->run();
 
